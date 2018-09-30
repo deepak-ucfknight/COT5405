4c74d4df260c1d3eff5429d7fe4553c6f045edc3
deepak-ucfknight <40718101+deepak-ucfknight@users.noreply.github.com>
2018-09-30 18:18:33 -0400

    Update COT5405.py

--- COT5405.py
+++ COT5405.py
@@ -1,9 +1,12 @@
 import random
 import timeit
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt  # remove this header if matplot is not installed in the system
+from memory_profiler import profile  # remove this header if memory_profiler is not installed in the system
 
 
 class Programming_Assignment1:
+
+    precision = 10
 
     def __init__(self, sequence_size):
         self.sequence_size = sequence_size
@@ -14,6 +17,9 @@ class Programming_Assignment1:
         return random.randint(minbound, maxbound)
 
     # Compute time complexity for this function only - Bitwise subtraction
+    # fp = open('memory_profiler_subtract.log', 'w+')  # comment this code when measuring time, else time will recorded for logging as well
+
+    # @profile(precision=precision, stream=fp) # comment this code when measuring time, else time will recorded for logging as well
     def subtract(self, sequence1, sequence2):
 
         if sequence1 < sequence2:
@@ -58,12 +64,15 @@ sequences = [4, 8, 16, 32, 64, 128, 256, 512]
 
 time = 0
 
+diff_time = []
 
-# operations = []
-#
-# summation_time = 0
-#
-# averagetimer = []
+time_array = []
+
+individual_array = []
+
+initialtime = 0
+
+summationtime = 0
 
 
 # Function to plot the performance
@@ -79,25 +88,26 @@ def graphplot(xaxis, yaxis, xlabel, ylabel, title):
 
 
 for sequence in sequences:
-    time = timeit.timeit(setup=SETUP_CODE.replace("***", str(sequence)), stmt=TEST_CODE, number=1000)
-    print("Compute time for sequence of size " + str(sequence) + " : " + str(time))
+    for x in range(1000):
+        time = timeit.timeit(setup=SETUP_CODE.replace("***", str(sequence)), stmt=TEST_CODE, number=1000) # change this value to 1 when measuring memory usage
+        summationtime += time
+        individual_array.append(time)
 
-# # time = timeit.timeit(setup=SETUP_CODE.replace("***", str(4)), stmt=TEST_CODE, number=1000)
-# # print("Compute time for sequence of size " + str(sequence) + " : " + str(time))
-#
-#
-# # Computing performance over 1000 iterations for 1000 random inputs of varied size of n
-# for sequence in sequences:
-#     for x in range(1000):
-#         time = timeit.timeit(setup=SETUP_CODE.replace("***", str(sequence)), stmt=TEST_CODE, number=1000)
-#         summation_time += time
-#         operations.append(time)
-#     # graphplot(range(1000), operations, 'Iterations', 'Time taken', 'Performance for sequence size ' + str(sequence) + ' over 1000 iterations')
-#     averagetimer.append(summation_time / 1000)
-#     summation_time = 0
-#     operations.clear()
-#     print("Compute time for sequence of size " + str(sequence) + " : " + str(time))
-# #
-# #
-# graphplot(sequences, averagetimer, 'Sequence', 'Average Compute Time', 'Order of growth of runtimes as the input scales')
+    avg = summationtime / 1000
+    print("Compute time for sequence of size " + str(sequence) + " : " + str(avg))
+    diff_time.append(abs(initialtime - avg))
+    time_array.append(avg)
+    initialtime = avg
+    graphplot(range(1000), individual_array, 'iterations', 'Time taken', 'Performance for sequence size ' + str(sequence) +' over million iterations')
+    individual_array.clear()
+
+
+
+for time in diff_time:
+    print("Increase in growth rate between previous and current iteration : " + str(time))
+
+graphplot(sequences, time_array, 'Sequence size', 'Time taken', 'Performance for sequence size  over million iterations')
+
+graphplot(sequences, diff_time, 'Sequence size', 'Time taken', 'Differntiable Growth rate over million iterations')
+
 

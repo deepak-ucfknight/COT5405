eedd821d1a90e548630df2a9293360a347614b85
deepak-ucfknight <40718101+deepak-ucfknight@users.noreply.github.com>
2018-10-01 18:30:38 -0400

    Update COT5405.py

--- COT5405.py
+++ COT5405.py
@@ -1,7 +1,28 @@
 import random
 import timeit
-import matplotlib.pyplot as plt  # remove this header if matplot is not installed in the system
-# from memory_profiler import profile  # remove this header if memory_profiler is not installed in the system
+import matplotlib.pyplot as plt      # remove this header if memory_profiler is not installed in the syste
+import sys
+
+
+# variables
+
+sequences = [4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
+
+time = 0
+
+diff_time = []
+
+time_array = []
+
+individual_array = []
+
+initial_time = 0
+
+summation_time = 0
+
+average_memory = []
+
+memory = []
 
 
 class Programming_Assignment1:
@@ -17,9 +38,7 @@ class Programming_Assignment1:
         return random.randint(minbound, maxbound)
 
     # Compute time complexity for this function only - Bitwise subtraction
-    # fp = open('memory_profiler_subtract.log', 'w+')  # comment this code when measuring time, else time will recorded for logging as well
 
-    # @profile(precision=precision, stream=fp) # comment this code when measuring time, else time will recorded for logging as well
     def subtract(self, sequence1, sequence2):
 
         if sequence1 < sequence2:
@@ -36,6 +55,8 @@ class Programming_Assignment1:
             sequence1 = sequence1 ^ sequence2  # Sum of all bits except 1 + 1 since carry has that.
 
             sequence2 = borrow << 1  # shifting carry
+
+        memory.append(sys.getsizeof(sequence1))
 
         return sequence1
 
@@ -58,24 +79,8 @@ sequence_2 = object_Programming_Assignment1.generate_random()
 
 '''
 
-# variables
-
-sequences = [4, 8, 16, 32, 64, 128, 256, 512]
-
-time = 0
-
-diff_time = []
-
-time_array = []
-
-individual_array = []
-
-initialtime = 0
-
-summationtime = 0
-
-
 # Function to plot the performance
+
 
 def graphplot(xaxis, yaxis, xlabel, ylabel, title):
     x = xaxis
@@ -87,27 +92,48 @@ def graphplot(xaxis, yaxis, xlabel, ylabel, title):
     plt.show()
 
 
+print("--------------------------------------------------")
+print("Time Complexity")
+print("---------------------------------------------------")
 for sequence in sequences:
     for x in range(1000):
         time = timeit.timeit(setup=SETUP_CODE.replace("***", str(sequence)), stmt=TEST_CODE, number=1000) # change this value to 1 when measuring memory usage
-        summationtime += time
+        summation_time += time
         individual_array.append(time)
+    average_memory.append(sum(memory) / len(memory))
+    memory.clear()
 
-    avg = summationtime / 1000
-    print(" Average Compute time for sequence of size " + str(sequence) + " : " + str(avg))
-    diff_time.append(abs(initialtime - avg))
+    avg = summation_time / 1000
+    print("Compute time for sequence of size " + str(sequence) + " : " + str(avg))
+    diff_time.append(abs(initial_time - avg))
     time_array.append(avg)
-    initialtime = avg
-    graphplot(range(1000), individual_array, 'iterations', 'Time taken', 'Performance for sequence size ' + str(sequence) +' over 1000 iterations')
+    initial_time = avg
+    # graphplot(range(1000), individual_array, 'iterations', 'Time taken', 'Performance for sequence size ' + str(sequence) +' over 1000 iterations')
     individual_array.clear()
 
+print("--------------------------------------------------")
+print(" Growth rate ")
+print("---------------------------------------------------")
 
+for growth in range(1, len(diff_time)):
+    print("Growth Factor for sequence of size " + str(sequences[growth]) + " : " + str(diff_time[growth] / diff_time[growth - 1]))
 
-for time in diff_time:
-    print("Increase in growth rate between previous and current iteration : " + str(time))
 
 graphplot(sequences, time_array, 'Sequence size', 'Time taken', 'Performance for sequence size  over 1000 iterations')
 
-# graphplot(sequences, diff_time, 'Sequence size', 'Time taken', 'Differential Growth rate over 1000 iterations')
+graphplot(sequences, diff_time, 'Sequence size', 'Time taken', 'Differentiable growth rate')
+
+print("--------------------------------------------------")
+print(" Space Complexity ")
+print("---------------------------------------------------")
+
+for mem_consumption in range(len(average_memory)):
+    print("Space allocated for sequence of size " + str(sequences[mem_consumption]) + " : " + str(average_memory[mem_consumption]))
+
+graphplot(sequences, average_memory, 'sequence structure', 'Space consumed', 'space complexity')
+
+
+
+
 
 
